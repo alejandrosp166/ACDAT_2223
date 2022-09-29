a15e7567a2f7e6061afe1306572affad8f43a5b2
alejandrosp16 <alejandrosecopineda@gmail.com>
2022-09-29 12:28:10 +0200

    Alejandro Seco Pineda

--- ad-t1-tablero/src/iesmm_ad_t1/TableroJuego.java
+++ ad-t1-tablero/src/iesmm_ad_t1/TableroJuego.java
@@ -7,24 +7,44 @@ public class TableroJuego {
     private int nfilas;
     private int ncolumnas;
     private Ficha[][] tablero;
-
+    // PREGUNTAR A ALEJANDRO SOBRE ESTE MÉTODO CONSTRUCTOR Y COMO PUEDE SER IMPLEMETENTADO PARA SU USO
     public TableroJuego(int nfilas, int ncolumnas) {
         this.nfilas = nfilas;
         this.ncolumnas = ncolumnas;
-        tablero = new Ficha[this.nfilas][this.ncolumnas];
+        this.tablero = new Ficha[this.nfilas][this.ncolumnas];
     }
 
     public TableroJuego(File f) {
-        Properties propiedades = new Properties();
+        Properties prop = new Properties();
+
         try {
             InputStream entrada = new FileInputStream(f);
-            propiedades.load(entrada);
+            prop.load(entrada);
         } catch (IOException e){
             e.printStackTrace();
         }
-        //CONTROLAR EXCEPCIÓN PARA QUE SEA UN NÚMERO EL INTEGER PARSE INT
-        tablero = new Ficha[Integer.parseInt(propiedades.getProperty("rows"))][Integer.parseInt(propiedades.getProperty("cols"))];
-        rellenarTablero(propiedades);
+
+        // CONTROLAR EXCEPCIÓN PARA QUE SEA UN NÚMERO EL INTEGER PARSE INT
+
+        this.nfilas = Integer.parseInt(prop.getProperty("rows"));
+        this.ncolumnas = Integer.parseInt(prop.getProperty("cols"));
+        this.tablero = new Ficha[nfilas][ncolumnas];
+
+        rellenarTablero(prop.getProperty("value1").charAt(0), prop.getProperty("value2").charAt(0));
+    }
+
+    public void rellenarTablero(char c1, char c2){
+
+        for (int i = 0; i < tablero.length;i++){
+            for (int j = 0; j < tablero[i].length; j++){
+
+                if((int)(Math.random() * 2) == 0){
+                    tablero[i][j] = new Ficha(c1);
+                } else{
+                    tablero[i][j] = new Ficha(c2);
+                }
+            }
+        }
     }
 
     @Override
@@ -39,19 +59,5 @@ public class TableroJuego {
             tableroFormado += "|\n";
         }
         return tableroFormado;
-    }
-
-    public void rellenarTablero(Properties prop){
-
-        for (int i = 0; i < tablero.length;i++){
-            for (int j = 0; j < tablero[i].length; j++){
-
-                if((int)(Math.random() * 2) == 0){
-                    tablero[i][j] = new Ficha(prop.getProperty("value1").charAt(0));
-                } else{
-                    tablero[i][j] = new Ficha(prop.getProperty("value2").charAt(0));
-                }
-            }
-        }
     }
 }
--- ad-t1-tablero/src/iesmm_ad_t1/TableroJuegoApp.java
+++ ad-t1-tablero/src/iesmm_ad_t1/TableroJuegoApp.java
@@ -21,17 +21,29 @@ package iesmm_ad_t1;
  */
 
 import java.io.*;
-import java.util.Properties;
 
 public class TableroJuegoApp {
+
+    //static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         File f = new File("res/tablero.props");
 
-        if (f.exists()){
-            TableroJuego tablero = new TableroJuego(f);
-            System.out.println(tablero.toString());
-        } else {
-            System.out.println("El fichero no existe en la ruta especificada");
+        if (!f.exists()){
+            System.out.println("El fichero no existe en la ruta especificada, se creará uno nuevo con valores por defecto...");
+            crearFicheroPropiedadesPorDefecto(f);
+        }
+
+        TableroJuego juego = new TableroJuego(f);
+        System.out.println(juego.toString());
+    }
+
+    private static void crearFicheroPropiedadesPorDefecto(File f){
+        try {
+            FileWriter fw = new FileWriter(f.getAbsolutePath());
+            fw.write("#Dimensiones del tablero\nrows=3\ncols=4\n#Fichas\nvalue1=\uD83D\uDD34\nvalue2=❌");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
