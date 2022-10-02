f3b28cd637c76aee7224a13f51701fe9714d3613
alejandrosp16 <alejandrosecopineda@gmail.com>
2022-10-02 21:08:01 +0200

    02/10/2022

--- ad-t1-tablero/src/iesmm_ad_t1/TableroJuego.java
+++ ad-t1-tablero/src/iesmm_ad_t1/TableroJuego.java
@@ -7,38 +7,36 @@ public class TableroJuego {
     private int nfilas;
     private int ncolumnas;
     private Ficha[][] tablero;
-    // PREGUNTAR A ALEJANDRO SOBRE ESTE MÉTODO CONSTRUCTOR Y COMO PUEDE SER IMPLEMETENTADO PARA SU USO
+
     public TableroJuego(int nfilas, int ncolumnas) {
+        // PREGUNTAR A ALEJANDRO SOBRE ESTE MÉTODO CONSTRUCTOR Y COMO PUEDE SER IMPLEMETENTADO PARA SU USO
         this.nfilas = nfilas;
         this.ncolumnas = ncolumnas;
         this.tablero = new Ficha[this.nfilas][this.ncolumnas];
     }
 
-    public TableroJuego(File f) {
+    public TableroJuego(File f) throws IOException, NegativeArraySizeException  {
+        // Leemos el fichero de propiedades
         Properties prop = new Properties();
+        InputStream input = new FileInputStream(f);
+        prop.load(input);
+        input.close();
 
-        try {
-            InputStream entrada = new FileInputStream(f);
-            prop.load(entrada);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-        // CONTROLAR EXCEPCIÓN PARA QUE SEA UN NÚMERO EL INTEGER PARSE INT
-
+        // Creamos el objeto de tipo tablero
         this.nfilas = Integer.parseInt(prop.getProperty("rows"));
         this.ncolumnas = Integer.parseInt(prop.getProperty("cols"));
         this.tablero = new Ficha[nfilas][ncolumnas];
 
+        // Rellenamos el tablero de manera aleatoria
         rellenarTablero(prop.getProperty("value1").charAt(0), prop.getProperty("value2").charAt(0));
     }
 
     public void rellenarTablero(char c1, char c2){
-
+        // Recorremos el tablero y le introducimos los caracteres de manera aleatoria
         for (int i = 0; i < tablero.length;i++){
             for (int j = 0; j < tablero[i].length; j++){
 
-                if((int)(Math.random() * 2) == 0){
+                if(Math.round((Math.random() * 1)) == 0){
                     tablero[i][j] = new Ficha(c1);
                 } else{
                     tablero[i][j] = new Ficha(c2);
@@ -49,7 +47,15 @@ public class TableroJuego {
 
     @Override
     public String toString() {
-        String tableroFormado = "";
+        // Usamos este bucle para crear la parte superior e inferior del tablero (+---+)
+        String supInf = "+ ";
+        for (int j =0;j < tablero.length; j++){
+            supInf += "- ";
+        }
+        supInf += "+";
+
+        // Creamos el tablero para mostrarlo por pantalla
+        String tableroFormado = supInf +"\n";
 
         for (int i = 0; i < tablero.length;i++){
             tableroFormado += "| ";
@@ -58,6 +64,6 @@ public class TableroJuego {
             }
             tableroFormado += "|\n";
         }
-        return tableroFormado;
+        return tableroFormado + supInf;
     }
 }
--- ad-t1-tablero/src/iesmm_ad_t1/TableroJuegoApp.java
+++ ad-t1-tablero/src/iesmm_ad_t1/TableroJuegoApp.java
@@ -9,7 +9,6 @@ package iesmm_ad_t1;
 
     Recuerda incluir el tratamiento de errores y posibles excepciones.
     EJEMPLO -> Valores negativos en el array
-    CREAR EL FICHERO CUANDO NO EXISTE Y RELLENARLO
 
     Representación generada:
     +-----------------+
@@ -26,15 +25,29 @@ public class TableroJuegoApp {
 
     //static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        File f = new File("res/tablero.props");
 
+        // Creamos el fichero que apuntará a la ruta del fichero de las propiedades
+        File f = new File("res" + File.separator + "tablero.props");
+
+        // Se comprueba si el fichero existe, si no existe se crea uno nuevo con valores por defecto
         if (!f.exists()){
             System.out.println("El fichero no existe en la ruta especificada, se creará uno nuevo con valores por defecto...");
             crearFicheroPropiedadesPorDefecto(f);
         }
 
-        TableroJuego juego = new TableroJuego(f);
-        System.out.println(juego.toString());
+        // Creamos el fichero tablero y lo mostramos por pantalla
+        try {
+            TableroJuego juego = new TableroJuego(f);
+            System.out.println(juego.toString());
+        } catch (IOException e){
+            // Control de excepción por si no podemos leer el fichero
+            System.err.println("Error al leer el fichero");
+            //e.printStackTrace();
+        } catch (NegativeArraySizeException e){
+            // Control de excepción por si intentamos crear un array con valores negativos
+            System.err.println("Las filas o las columnas del tablero no pueden ser un valor negativo");
+            //e.printStackTrace();
+        }
     }
 
     private static void crearFicheroPropiedadesPorDefecto(File f){
@@ -43,6 +56,7 @@ public class TableroJuegoApp {
             fw.write("#Dimensiones del tablero\nrows=3\ncols=4\n#Fichas\nvalue1=\uD83D\uDD34\nvalue2=❌");
             fw.close();
         } catch (IOException e) {
+            System.err.println("No se pudo crear el fichero de propiedades");
             e.printStackTrace();
         }
     }
