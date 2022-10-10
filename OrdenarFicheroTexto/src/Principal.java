df0b689283c1c105e3ba465b025acb0f3f7da6e9
alejandrosp16 <alejandrosecopineda@gmail.com>
2022-10-10 23:36:54 +0200

    10/10/22

--- OrdenarFicheroTexto/src/Principal.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-
-public class Principal {
-    public static void main(String[] args) {
-        File f = OrdenarArchivoTexto.sort(new File("res" + File.separator + "fichero.txt"));
-    }
-}
