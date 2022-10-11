d443d34c937d7c2f869badf7e08ec85c2f2e3eeb
alejandrosp166 <asecpin2303@iesmartinezm.es>
2022-10-11 19:32:58 +0200

    11/10/22

--- ACD_t1_Proyecto1y2/src/proyecto2/GestionaClientes.java
+++ ACD_t1_Proyecto1y2/src/proyecto2/GestionaClientes.java
@@ -4,10 +4,9 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class GestionaClientes {
-    public boolean generarFichero(String fcsv){
+    public boolean generarFichero(String fcsv) {
         // COMPROBAR SI EXISTE EL FICHERO TAMBIÉN (CONTROL DE ERRORES)
-        // LÍNEA 21 SEPARAR LA CADENA Y ALMACENAR LOS DATOS EN EL ARRAY
-        if (fcsv.matches("NO TIENE .CSV")){
+        if (fcsv.matches("NO TIENE .CSV")) {
             System.err.println("El fichero debe tener la extensión .csv");
             return false;
         }
@@ -18,7 +17,8 @@ public class GestionaClientes {
             String lineaCli = leer.readLine();
 
             while (lineaCli != null) {
-                clientes.add(new Cliente("pepe",234));
+                String[] sep = lineaCli.split(",");
+                clientes.add(new Cliente(sep[0] + " " + sep[1] + " " + sep[2], Long.parseLong(sep[3])));
                 lineaCli = leer.readLine();
             }
             leer.close();
@@ -32,20 +32,30 @@ public class GestionaClientes {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
         return true;
     }
 
-    public void mostrarFichero(File f){
+    public void mostrarFichero(File f) {
+        if(f.exists()){
+            System.out.println("Ruta del archivo: " + f.getAbsolutePath());
+            // Deserializar, meter en un array
+            ArrayList<Cliente> clientes = new ArrayList<>();
 
+            for (Cliente c : clientes){
+                String[] nombreSep = c.getNombre().split(" ");
+                System.out.println(c.getTelefono() + nombreSep[0] + "   " + nombreSep[1] + "," + nombreSep[3]);
+            }
+
+        } else {
+            System.err.println("El fichero no existe");
+        }
     }
 
-    public boolean ordenarPorNombre(File f){
+    public boolean ordenarPorNombre(File f) {
         return true;
     }
 
-    public void duplicados(File f1, File f2, String path){
+    public void duplicados(File f1, File f2, String path) {
 
     }
 
