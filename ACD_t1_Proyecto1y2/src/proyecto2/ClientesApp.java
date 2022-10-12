13ad236cc3b4c0358ced3056dfffb05a36714c39
alejandrosp16 <alejandrosecopineda@gmail.com>
2022-10-12 20:49:58 +0200

    12/10/22

--- ACD_t1_Proyecto1y2/src/proyecto2/ClientesApp.java
+++ ACD_t1_Proyecto1y2/src/proyecto2/ClientesApp.java
@@ -1,7 +1,23 @@
 package proyecto2;
 
-public class ClientesApp {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.Scanner;
 
+public class ClientesApp {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Escribe el fichero que quieres mostrar, solo dos opciones (clientes1.csv // clientes2.csv)");
+        String fileName = sc.nextLine();
+        File f = new File("res" + File.separator + fileName);
+
+        if (GestionaClientes.generarFichero(f.getAbsolutePath())){
+            System.out.println("El fichero se ha generado con éxito, ¿Quieres mostrarlo? (y/n)");
+            if (sc.nextLine().equals("y")){
+                GestionaClientes.mostrarFichero(new File("res" + File.separator + fileName));
+            }
+
+        } else {
+            System.err.println("Error al generar el fichero");
+        }
     }
 }
--- ACD_t1_Proyecto1y2/src/proyecto2/GestionaClientes.java
+++ ACD_t1_Proyecto1y2/src/proyecto2/GestionaClientes.java
@@ -4,58 +4,87 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class GestionaClientes {
-    public boolean generarFichero(String fcsv) {
-        // COMPROBAR SI EXISTE EL FICHERO TAMBIÉN (CONTROL DE ERRORES)
-        if (fcsv.matches("NO TIENE .CSV")) {
-            System.err.println("El fichero debe tener la extensión .csv");
+    public static boolean generarFichero(String fcsv) {
+        if (!fcsv.endsWith(".csv") || !new File(fcsv).exists()) {
+            // Controlar que el fichero que se va a serializar sea con la extensión .csv
+            System.err.println("El fichero no existe o no tiene la extensión .csv");
             return false;
         }
+        // Declaramos el array que guardará los datos de los clientes
+        ArrayList<Cliente> clients = new ArrayList<>();
 
-        ArrayList<Cliente> clientes = new ArrayList<>();
         try {
+            // Creamos el flujo de lectura del fichero
             BufferedReader leer = new BufferedReader(new FileReader(new File(fcsv)));
+            // Leemos la primera línea
             String lineaCli = leer.readLine();
 
+            // Leemos la líneas hasta que se termine el fichero
             while (lineaCli != null) {
+                // Guardamos los datos de manera separa en un array con la función split
                 String[] sep = lineaCli.split(",");
-                clientes.add(new Cliente(sep[0] + " " + sep[1] + " " + sep[2], Long.parseLong(sep[3])));
+                // Creamos el cliente y lo guardamos en el array
+                clients.add(new Cliente(sep[0] + " " + sep[1] + " " + sep[2], Long.parseLong(sep[3])));
+                // Leemos la siguiente línea
                 lineaCli = leer.readLine();
             }
+            // Cerramos el flujo de lectura
             leer.close();
-
+            // Generamos el mismo fichero en la ruta que nos han pasado por parámetro pero esta vez con la extensión .dat
             ObjectOutputStream ser = new ObjectOutputStream(new FileOutputStream(fcsv.replaceFirst(".csv", ".dat")));
-            ser.writeObject(clientes);
+            // Y le escribimos los datos serializados
+            ser.writeObject(clients);
+            // Cerramos el flujo de escritura
             ser.close();
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            // Controlar excepción en caso de que el fichero no se pueda leer
+            System.err.println("Error a la hora de leer el fichero");
+            //e.printStackTrace();
         }
         return true;
     }
-
-    public void mostrarFichero(File f) {
-        if(f.exists()){
+    public static void mostrarFichero(File f) {
+        // Comprobamos que el fichero exista y que termine en la extensión .dat
+        if(f.exists() && f.getAbsolutePath().endsWith(".dat")){
+            // Mostramos la ruta del archivo
             System.out.println("Ruta del archivo: " + f.getAbsolutePath());
-            // Deserializar, meter en un array
-            ArrayList<Cliente> clientes = new ArrayList<>();
+            try {
+                // Creamos el flujo de lectura
+                ObjectInputStream leer = new ObjectInputStream(new FileInputStream(f));
+                // Creamos el array donde guardaremos los datos
+                ArrayList<Cliente> clients = (ArrayList<Cliente>) leer.readObject();
 
-            for (Cliente c : clientes){
-                String[] nombreSep = c.getNombre().split(" ");
-                System.out.println(c.getTelefono() + nombreSep[0] + "   " + nombreSep[1] + "," + nombreSep[3]);
+                for (Cliente c : clients){
+                    // Separamos el nombre para poder poner la coma en el segundo espacio
+                    String[] nameSep = c.getNombre().split(" ");
+                    // Mostramos los datos y pasamos los nombres a mayúsculas
+                    System.out.println(c.getTelefono() + "   " + (nameSep[0] + " " + nameSep[1] + ", " + nameSep[2]).toUpperCase());
+                }
+            } catch (IOException e) {
+                // Controlar excepción en caso de que el fichero no se pueda leer
+                System.err.println("Error a la hora de leer el fichero");
+                //e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                // Controlar la excepción
+                System.err.println("");
+                //e.printStackTrace();
             }
-
         } else {
-            System.err.println("El fichero no existe");
+            System.err.println("El fichero no existe o no tiene la extensión .dat");
         }
     }
 
-    public boolean ordenarPorNombre(File f) {
+    public static boolean ordenarPorNombre(File f) {
+        if (!f.exists() && !f.getAbsolutePath().endsWith(".dat")){
+            // Controlar que el fichero que se va a ordenar termine en .dat
+            System.err.println("El fichero no existe o no tiene la extensión .dat");
+            return false;
+        }
         return true;
     }
 
-    public void duplicados(File f1, File f2, String path) {
+    public static void duplicados(File f1, File f2, String path) {
 
     }
 
